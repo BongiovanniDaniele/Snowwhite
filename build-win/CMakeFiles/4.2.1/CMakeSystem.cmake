3e3a179f439f2f80278d38fbc85d10c83af4b9c8
alibucco <alicebuck04@gmail.com>
2026-01-04 18:58:01 +0100

    Aggiornamento gitignore per fix build

---
+++ build-win/CMakeFiles/4.2.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-25.1.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "25.1.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/alicebuccoleri/Desktop/OpenGL-Project-main/cmake-build-debug/mingw-toolchain.cmake")
+
+set(CMAKE_SYSTEM "Windows")
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
